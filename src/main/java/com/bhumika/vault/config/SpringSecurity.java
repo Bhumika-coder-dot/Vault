230b4d49774d8e51608ff59fae4c80336c4ac504
Bhumika Kolekar <your-email@gmail.com>
2026-06-11 11:17:41 +0530

    Remove dev profile from security config

--- src/main/java/com/bhumika/vault/config/SpringSecurity.java
+++ src/main/java/com/bhumika/vault/config/SpringSecurity.java
@@ -19,7 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@Profile("dev")
+
 public class SpringSecurity {
 
     private final UserDetailsServiceImpl userDetailsService;
